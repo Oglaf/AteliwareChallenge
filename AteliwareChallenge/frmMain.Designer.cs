8244a2d9401c4d0f418bae80025fd1b597171c0b
Christian Fleishmann Silva <chrisfleishmann@outlook.com>
2020-08-09 16:08:50 -0300

    Commiting files for the first time.

---
+++ AteliwareChallenge/frmMain.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace AteliwareChallenge
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mStripMain = new System.Windows.Forms.MenuStrip();
+            this.getRepoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewRepoDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gridSearchResult = new System.Windows.Forms.DataGridView();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.uRLDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.loginDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.searchResultsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.gitRepoDataSet = new AteliwareChallenge.GitRepoDataSet();
+            this.searchResultsTableAdapter = new AteliwareChallenge.GitRepoDataSetTableAdapters.SearchResultsTableAdapter();
+            this.mStripMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridSearchResult)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchResultsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gitRepoDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mStripMain
+            // 
+            this.mStripMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.getRepoToolStripMenuItem,
+            this.viewRepoDetailsToolStripMenuItem});
+            this.mStripMain.Location = new System.Drawing.Point(0, 0);
+            this.mStripMain.Name = "mStripMain";
+            this.mStripMain.Size = new System.Drawing.Size(673, 24);
+            this.mStripMain.TabIndex = 0;
+            // 
+            // getRepoToolStripMenuItem
+            // 
+            this.getRepoToolStripMenuItem.Name = "getRepoToolStripMenuItem";
+            this.getRepoToolStripMenuItem.Size = new System.Drawing.Size(134, 20);
+            this.getRepoToolStripMenuItem.Text = "Search and Save Repo";
+            this.getRepoToolStripMenuItem.Click += new System.EventHandler(this.getRepoToolStripMenuItem_Click);
+            // 
+            // viewRepoDetailsToolStripMenuItem
+            // 
+            this.viewRepoDetailsToolStripMenuItem.Name = "viewRepoDetailsToolStripMenuItem";
+            this.viewRepoDetailsToolStripMenuItem.Size = new System.Drawing.Size(108, 20);
+            this.viewRepoDetailsToolStripMenuItem.Text = "View repo details";
+            this.viewRepoDetailsToolStripMenuItem.Click += new System.EventHandler(this.viewRepoDetailsToolStripMenuItem_Click);
+            // 
+            // gridSearchResult
+            // 
+            this.gridSearchResult.AllowUserToAddRows = false;
+            this.gridSearchResult.AllowUserToDeleteRows = false;
+            this.gridSearchResult.AutoGenerateColumns = false;
+            this.gridSearchResult.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gridSearchResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridSearchResult.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.uRLDataGridViewTextBoxColumn,
+            this.loginDataGridViewTextBoxColumn});
+            this.gridSearchResult.DataSource = this.searchResultsBindingSource;
+            this.gridSearchResult.Location = new System.Drawing.Point(0, 27);
+            this.gridSearchResult.Name = "gridSearchResult";
+            this.gridSearchResult.ReadOnly = true;
+            this.gridSearchResult.Size = new System.Drawing.Size(661, 274);
+            this.gridSearchResult.TabIndex = 1;
+            this.gridSearchResult.SelectionChanged += new System.EventHandler(this.gridSearchResult_SelectionChanged);
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // uRLDataGridViewTextBoxColumn
+            // 
+            this.uRLDataGridViewTextBoxColumn.DataPropertyName = "URL";
+            this.uRLDataGridViewTextBoxColumn.HeaderText = "URL";
+            this.uRLDataGridViewTextBoxColumn.Name = "uRLDataGridViewTextBoxColumn";
+            this.uRLDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // loginDataGridViewTextBoxColumn
+            // 
+            this.loginDataGridViewTextBoxColumn.DataPropertyName = "Login";
+            this.loginDataGridViewTextBoxColumn.HeaderText = "Login";
+            this.loginDataGridViewTextBoxColumn.Name = "loginDataGridViewTextBoxColumn";
+            this.loginDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // searchResultsBindingSource
+            // 
+            this.searchResultsBindingSource.DataMember = "SearchResults";
+            this.searchResultsBindingSource.DataSource = this.gitRepoDataSet;
+            // 
+            // gitRepoDataSet
+            // 
+            this.gitRepoDataSet.DataSetName = "GitRepoDataSet";
+            this.gitRepoDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // searchResultsTableAdapter
+            // 
+            this.searchResultsTableAdapter.ClearBeforeFill = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(673, 303);
+            this.Controls.Add(this.gridSearchResult);
+            this.Controls.Add(this.mStripMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.mStripMain;
+            this.Name = "frmMain";
+            this.Text = "Ateliware Challenge";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.mStripMain.ResumeLayout(false);
+            this.mStripMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridSearchResult)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchResultsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gitRepoDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mStripMain;
+        private System.Windows.Forms.ToolStripMenuItem getRepoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewRepoDetailsToolStripMenuItem;
+        private System.Windows.Forms.DataGridView gridSearchResult;
+        private GitRepoDataSet gitRepoDataSet;
+        private System.Windows.Forms.BindingSource searchResultsBindingSource;
+        private GitRepoDataSetTableAdapters.SearchResultsTableAdapter searchResultsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn uRLDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn loginDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ AteliwareChallenge/frmMain.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Octokit;
+
+namespace AteliwareChallenge
+{
+    public partial class frmMain : Form
+    {
+        string connectionString;
+        string name;
+        string login;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["AteliwareChallenge.Properties.Settings.GitRepoConnectionString"].ConnectionString;
+        }
+
+        private void getRepoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            getResults();
+            LoadRecords();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'gitRepoDataSet.SearchResults' table. You can move, or remove it, as needed.
+            this.searchResultsTableAdapter.Fill(this.gitRepoDataSet.SearchResults);
+            LoadRecords();
+        }
+
+        private void getResults()
+        {
+            var searchClient = new SearchClient(new ApiConnection(new Connection(new ProductHeaderValue("SearchRepo"))));
+            var searchRepositoriesRequest = new SearchRepositoriesRequest
+            {
+                Language = Language.CSharp,
+                Forks = Range.GreaterThan(400),
+                SortField = RepoSearchSort.Stars,
+                Order = SortDirection.Descending
+            };
+
+            var searchRepositoryResult = searchClient.SearchRepo(searchRepositoriesRequest).Result.Items.ToList();
+
+            foreach (var result in searchRepositoryResult.Take(5))
+            {
+                InsertResult(result.Name, result.HtmlUrl, result.Owner.Login);
+            }
+        }
+
+        private void InsertResult(string _name, string _url, string _login)
+        {
+            byte[] zipFile = DownloadProjectsFromGitHub(_name, _url);
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = " IF NOT EXISTS (SELECT * FROM SearchResults WHERE url = @url) \n\r" +
+                " BEGIN \n\r" +
+                "     INSERT INTO SearchResults(name, url, repository, login) VALUES (@name, @url, @repository, @login) \n\r" +
+                " SELECT SCOPE_IDENTITY() \n\r " +
+                " END \n\r " +
+                " ELSE SELECT 0";
+
+                using (
+                    SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@name", _name);
+                    command.Parameters.AddWithValue("@url", _url);
+                    command.Parameters.AddWithValue("@login", _login);
+                    command.Parameters.AddWithValue("@repository", zipFile);
+
+                    connection.Open();
+                    int result = command.ExecuteNonQuery();
+
+                    if (result < 0)
+                    {
+                        Console.WriteLine("Error inserting data into Database!");
+                    }
+
+                }
+            }
+        }
+
+        private byte[] DownloadProjectsFromGitHub(string _name, string _url)
+        {
+            Regex illegalInFileName = new Regex(@"[\\/:*?""<>|]");
+            string name = illegalInFileName.Replace(_name, "");
+
+            var archivePath = Path.Combine(GetBuildFolder(), name + ".zip");
+            var destinationDirectoryName = Path.Combine(GetBuildFolder(), name);
+            if (!File.Exists(archivePath))
+            {
+                var downloadUrl = _url + "/archive/master.zip";
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile(downloadUrl, archivePath);
+                }
+            }
+            byte[] bytes = System.IO.File.ReadAllBytes(archivePath);
+            File.Delete(archivePath); ;
+
+            return bytes;
+        }
+
+        private void LoadRecords()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM SearchResults", connection))
+            {
+                connection.Open();
+
+                DataTable searchResultsTable = new DataTable();
+                adapter.Fill(searchResultsTable);
+                gridSearchResult.DataSource = searchResultsTable;
+            }
+        }
+
+        private static string GetBuildFolder()
+        {
+            string folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (!Directory.Exists(folder))
+                Directory.CreateDirectory(folder);
+            return folder;
+        }
+
+        private void viewRepoDetailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(name) )
+            {
+                MessageBox.Show("Please select a repository.");
+            }
+            else
+            {
+                frmDetails form = new frmDetails(login, name);
+                form.Show();
+            }
+        }
+
+        private void gridSearchResult_SelectionChanged(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in gridSearchResult.SelectedRows)
+            {
+                name = row.Cells[0].Value.ToString();
+                login = row.Cells[2].Value.ToString();
+            }
+        }
+    }
+}
